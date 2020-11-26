31ee93fec4e046c6a9fe246a7e3966dd0c4fabee
MK <MK@DESKTOP-9UJPVHE>
2020-11-26 12:26:21 +0800

    20201126添加按钮

--- myOCR/MainForm.Designer.cs
+++ myOCR/MainForm.Designer.cs
@@ -36,6 +36,7 @@
             this.btn_WriteValue = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btn_ReadValue = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -92,11 +93,21 @@
             this.btn_ReadValue.UseVisualStyleBackColor = true;
             this.btn_ReadValue.Click += new System.EventHandler(this.btn_ReadValue_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(603, 490);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_ReadValue);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.btn_WriteValue);
@@ -120,6 +131,7 @@
         private System.Windows.Forms.Button btn_WriteValue;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button btn_ReadValue;
+        private System.Windows.Forms.Button button1;
     }
 }
 
